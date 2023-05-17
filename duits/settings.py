1be46c80677f0cd93bf32b8add44ba1ced319294
karmakersagar <karmakersagar25@gmail.com>
2023-05-18 01:27:44 +0600

    fiix

--- duits/settings.py
+++ duits/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l2q%+vukbdiqx2pzw)$wp(fdksmjqgkr)ms8c-6j!(*l1c3fnh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
